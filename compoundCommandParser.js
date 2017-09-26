35df8caee51624aaeac08cd124afb59279def048
Alpvax <alpvax@gmail.com>
2017-09-26 14:02:19 +0100

    Begin CompoundCommand rewrite
    Added return keybind

---
+++ compoundCommandParser.js
@@ -0,0 +1,58 @@
+function CommandParser(inputText)
+{
+    this.inputText = inputText;
+    this.currentText = inputText.value;
+    this.active = true;
+	this.hotkeys = {};
+    this.activate = function()
+    {
+        this.active = true;
+        return this;
+    }.bind(this);
+    this.deactivate = function()
+    {
+        this.active = false;
+        return this;
+    }.bind(this);
+    this.setText = function(text, displayedOnly)
+    {
+        this.inputText.value = text;
+		if(!displayedOnly)
+		{
+			this.currentText = text;
+		}
+    }.bind(this);
+	this.addHotkey = function(key, callback)
+	{
+		if(key in this.hotkeys)
+		{
+			console.log("Replacing keybind \"%s\": %O with: %O", key, this.hotkeys[key], callback);
+		}
+		this.hotkeys[key] = callback;
+	}.bind(this);
+    this.clearText = function()
+    {
+        this.setText("", false);
+    }.bind(this);
+	this.__handleKeyDown = function(e)
+    {
+        if(!this.active)
+        {
+            return;
+        }
+        if(e.key in this.hotkeys)
+        {
+			var result = this.hotkeys[e.key](e, this.inputText.value, this);
+        }
+    }.bind(this);
+	this.addHotkey("Enter", returnKeyBind)
+    this.inputText.addEventListener("keydown", this.__handleKeyDown);
+}
+	
+function returnKeyBind(e, input, parser)
+{
+	e.preventDefault();
+	//TODO:parser.parseAndSubmit(inputText.value);
+	console.log(input);
+	parser.clearText();
+}
--- jsctest.js
+++ jsctest.js
@@ -2,6 +2,15 @@ function load()
 {
     var input = document.getElementById("textInput");
     var parser = new CommandParser(input);
+}
+
+
+
+
+function loadAutoComplete()//Not called
+{
+    var input = document.getElementById("textInput");
+    var parser = new CommandParser(input);
     
     parser.register(new Command(/\w+(\s+(\w+))*/i, function(e, inputText, parser)
     {
@@ -14,19 +23,4 @@ function load()
         console.log("Wish upon a star!");
         return true;
     }, "wish", "hope", "pray"));
-}
-
-function parseInput(e)
-{
-    var input = e.srcElement;
-    if(e.key.toLowerCase() =="enter")
-    {
-        e.preventDefault();
-        input.value = "";
-    }
-    if(e.key.toLowerCase() =="tab")
-    {
-        e.preventDefault();
-    }
-    console.log(e);
 }
